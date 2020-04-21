ce84dccd0d1274fbaca6eda23c91a76c958638fc
Aziz Sonawalla <sonawallaa@gmail.com>
2020-04-20 20:56:03 -0400

    setup tests and add some stubs

--- src/GraphParser.ts
+++ src/GraphParser.ts
@@ -1,7 +1,12 @@
+export interface Graph {
+    adjacency_matrix: Array<Array<number>>
+    vertices: Array<string>
+}
+
 /**
  * Reads a graph input from a text file and returns an adjacency matrix
  * @param path path to the text file
  */
-export function read(path: string): Array<Array<number>> {
+export function read(path: string): Graph {
     throw new Error("Not Implemented")
 }
---
+++ test/GraphParser.spec.ts
@@ -0,0 +1,23 @@
+import {expect} from "chai";
+
+describe("Graph Parser", function () {
+
+    before(function () {
+    });
+
+    after(function () {
+    });
+
+    beforeEach(function () {
+        // might want to add some process logging here to keep track of what"s going on
+    });
+
+    afterEach(function () {
+        // might want to add some process logging here to keep track of what"s going on
+    });
+
+    it("should read empty graph", function () {
+        expect(true)
+    });
+
+})
