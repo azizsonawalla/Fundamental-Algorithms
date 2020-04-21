31e21e9919131969c8bcd53f448d17403509d5bc
Aziz Sonawalla <sonawallaa@gmail.com>
2020-04-20 20:15:39 -0400

    initial setup and files

---
+++ src/GraphParser.ts
@@ -0,0 +1,7 @@
+/**
+ * Reads a graph input from a text file and returns an adjacency matrix
+ * @param path path to the text file
+ */
+export function read(path: string): Array<Array<number>> {
+    throw new Error("Not Implemented")
+}
